8b36ec7dc9acbc81eb7d8a3aa0cc67666d0f73c0
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2022-07-07 13:40:11 -0500

    <backend> se mejoro el evaluation

--- src/backend/game/Board.cpp
+++ src/backend/game/Board.cpp
@@ -1,4 +1,5 @@
 #include "Board.h"
+#include <cmath>
 
 
 chess::Board::Board() : pieceFactory(std::make_shared<PieceFactory>()) {
@@ -308,8 +309,24 @@ chess::ScoreType chess::Board::evaluation() {
             if (piece != nullptr) {
                 if (piece->getColor() == BLACK) {
                     blackPoints += piece->getValue();
+                    if (std::dynamic_pointer_cast<Pawn>(piece) != nullptr) {
+                        if ((i >= BOARD_SIZE / 2 && i <= BOARD_SIZE / 2 + 1) || (j >= BOARD_SIZE / 2 && j <= BOARD_SIZE / 2 + 1)) {
+                            blackPoints += 50;
+                        }
+                    }
+                    if (std::dynamic_pointer_cast<King>(piece) != nullptr) {
+                        whitePoints += (std::pow(j - BOARD_SIZE / 2, 2) - std::pow(j, 2)) * 4;
+                    }
                 } else {
                     whitePoints += piece->getValue();
+                    if (std::dynamic_pointer_cast<Pawn>(piece) != nullptr) {
+                        if ((i >= BOARD_SIZE / 2 && i <= BOARD_SIZE / 2 + 1) || (j >= BOARD_SIZE / 2 && j <= BOARD_SIZE / 2 + 1)) {
+                            whitePoints += 50;
+                        }
+                    }
+                    if (std::dynamic_pointer_cast<King>(piece) != nullptr) {
+                        blackPoints += (std::pow(j - BOARD_SIZE / 2, 2) - std::pow(j, 2)) * 4;
+                    }
                 }
             }
         }
